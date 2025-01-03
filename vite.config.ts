5ffe4645c0a318510d83a76732655dad5ea6bc38
Ismoil <admin@ismoil.site>
2025-01-04 01:39:06 +0300

    Added port on preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  preview: {
+    port: 3000
+  },
   server: {
     port: 3000
   },
